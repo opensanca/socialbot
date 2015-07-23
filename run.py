2eaf5ad84a3d8b801ba085e9a232b31651e14ef5
Lucas Marques <marquesds@users.noreply.github.com>
2015-07-23 10:44:27 -0300

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
             for channel in bot.channels():
                 bot.listen(channel['id'])
         except Exception as ex:
-            bot = SocialBot()
             error_message = "Amiguinhos, tive um problema. Podem me ajudar? Erro: %s", ex
             bot.slack.chat.post_message(channel='#general',
                                         text=error_message,
