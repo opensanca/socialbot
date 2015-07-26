945ee76867d011610e02a21f96650b4242128a98
Lucas Marques <lucasmarquesds@gmail.com>
2015-07-26 19:44:32 -0300

    settings example

---
+++ settings.py
@@ -0,0 +1,22 @@
+# Keep it secret
+SECRET_KEY = ''
+
+# Social tokens
+FACEBOOK = {
+    'token': '',
+    'app_id': '',
+    'app_secret': ''
+}
+
+TWITTER = {
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token': '',
+    'access_token_secret': ''
+}
+
+# Bot name
+BOT_NAME = ''
+
+# Say bot to share something
+SHARE_TRIGGER = '@share'
