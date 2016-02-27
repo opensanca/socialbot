8b6270a8a33cf0cc69502bb00e16c670d924aa21
Lucas Marques <lucasmarquesds@gmail.com>
2016-02-27 08:39:12 -0300

    removing insecure informations

--- settings.py
+++ settings.py
@@ -1,18 +1,18 @@
 # Keep it secret
-SECRET_KEY = 'xoxb-9470404417-NrM4ZFtnT77J9lJKOIocmhJh'
+SECRET_KEY = ''
 
 # Social tokens
 FACEBOOK = {
-    'token': 'CAATr2hXT0j8BAB6hhcDu9PZBHn5VBYPwgRnZBa4ib0SRicmAIreqUnZBcO5gmZBLpFFS8apT8ulp9zXmw4SQrZAZCmtCE7vZCKYGVlQKThraX9km9ZAgcQBGMpZBNdxFJksPfgYePDDDSERbhMgpmnrPjYvZBSq2hcWhNf8ivFRbYtWsSthUZBKhlJFedo6ZCM1pSAWjFqHcx4sttQZDZD',
-    'app_id': '1385221808443967',
-    'app_secret': 'ded906daa0d7176b70e1e422113d9382'
+    'token': '',
+    'app_id': '',
+    'app_secret': ''
 }
 
 TWITTER = {
-    'consumer_key': 'LwAteNDyGjU4m8CdSs3vgPHFq',
-    'consumer_secret': 'We5WQTXyuSBmzwP8mCyrKWCOV18wIi0tAbLMSmy7NXSPDH34JC',
-    'access_token': '3252729469-aaRNAdzneOQuNgeKEenJqY2zq7ZrvmPnoOEav3s',
-    'access_token_secret': '82HfhGApkk82dMCUOLxMpTq8VVBBQcyLKnJq9MH0zhtUh'
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token': '',
+    'access_token_secret': ''
 }
 
 # Bot name
