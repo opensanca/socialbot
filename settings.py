3d820d5d858b41c9d8a9eb45dcdaa37e279964d6
Lucas Marques <lucasmarquesds@gmail.com>
2016-03-03 22:53:52 -0300

    rewriting the bot

--- settings.py
+++ settings.py
@@ -2,13 +2,6 @@ import json
 
 
 def get_secret(api, secret):
-    """
-    Return
-    Args:
-        api: asd
-        secret: asd
-    return: asd
-    """
     with open("secrets.json") as f:
         content = f.read()
     return json.loads(content).get(api).get(secret)
